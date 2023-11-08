be5e4ee3f568992d54fe14ef1e3ca795aca07f81
root <ichraf.sassi.456@gmail.com>
2023-11-07 18:37:18 -0800

    fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Ability to inquire about subscribers on specific Red subreddit."""
+""" the Function to query subscribers on a given Reddit subreddit."""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Give back total number of members on a specific subreddit."""
-    lin = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """Return  total number of subscribers on  given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(lin, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
